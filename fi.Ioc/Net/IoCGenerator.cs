6f593f7a189d81e1dd1b9ed09745debf637c0cad
emre.yesilcimen <yesilcimen@hotmail.com>
2026-05-02 00:58:39 +0300

    InjectAppNameAttribute enhancement has been integrated into fi.Ioc.

--- fi.Ioc/Net/IoCGenerator.cs
+++ fi.Ioc/Net/IoCGenerator.cs
@@ -7,134 +7,181 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using fi.Core;
+using fi.Core.AbstractUtility;
 
-namespace fi.Ioc
+namespace fi.Ioc;
+
+public sealed class IoCGenerator
 {
-    public sealed class IoCGenerator
+    public class DoTNet : LazySingleton<DoTNet>
     {
-        public class DoTNet
+        private readonly IDictionary<Type, Action<Type, IServiceCollection>> GetLifeCycle =
+            new Dictionary<Type, Action<Type, IServiceCollection>>
+            {
+                {
+                    typeof(ISingletonDependency),
+                    (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces
+                        .Where(m => m != typeof(ISingletonDependency) && !m.Namespace.Equals("fi.Framework") &&
+                                    !m.Namespace.Contains("System")).ToList()
+                        .ForEach(i => service.AddSingleton(i, implementationType))
+                },
+                {
+                    typeof(IScopedDependency),
+                    (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces
+                        .Where(m => m != typeof(IScopedDependency) && !m.Namespace.Equals("fi.Framework") &&
+                                    !m.Namespace.Contains("System")).ToList()
+                        .ForEach(i => service.AddScoped(i, implementationType))
+                },
+                {
+                    typeof(ITransientDependency),
+                    (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces
+                        .Where(m => m != typeof(ITransientDependency) && !m.Namespace.Equals("fi.Framework") &&
+                                    !m.Namespace.Contains("System")).ToList()
+                        .ForEach(i => service.AddTransient(i, implementationType))
+                },
+                {
+                    typeof(IScopedSelfDependency),
+                    (implementationType, service) => service.AddScoped(implementationType)
+                },
+                {
+                    typeof(ISingletonSelfDependency),
+                    (implementationType, service) => service.AddSingleton(implementationType)
+                },
+                {
+                    typeof(ITransientSelfDependency),
+                    (implementationType, service) => service.AddTransient(implementationType)
+                }
+            };
+
+        internal IServiceCollection Services { get; private set; }
+        internal IConfiguration Configuration { get; private set; }
+
+        /// <summary>
+        /// .Net in standart IOC  kullanımı için geliştirilmiştir.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public void Start(IServiceCollection services, IConfiguration configuration)
         {
-            private static readonly Lazy<DoTNet> lazy = new(() => new DoTNet());
-            public static DoTNet Current => lazy.Value;
-            private DoTNet()
+            Services = services;
+            Configuration = configuration;
+            RegisterInterfaceBasedTypes();
+            ConfigureOptionsCore();
+        }
+
+        /// <summary>
+        /// .Net in standart Worker ları için geliştirilmiştir.
+        /// </summary>
+        /// <param name="hostContext"></param>
+        /// <param name="services"></param>
+        public void Start(HostBuilderContext hostContext, IServiceCollection services)
+        {
+            Services = services;
+            Configuration = hostContext.Configuration;
+            RegisterInterfaceBasedTypes();
+            ConfigureOptionsCore();
+            RegisterHostingWorker();
+        }
+
+        /// <summary>
+        /// ITransientDependency,IScopedDependency veya ISingletonDependency nesneleri konteynıra ekliyor.
+        /// </summary>
+        private void RegisterInterfaceBasedTypes()
+        {
+            var dependencyObjects = AppDomain.Instance.GetAllAssemblies().SelectMany(s =>
+                s.DefinedTypes.Where(w => !w.IsAbstract && w.IsClass).Select(sm => sm));
+            var appName = Assembly.GetEntryAssembly().GetName().Name;
+
+            var pureObjects = dependencyObjects.Where(w => (typeof(ITransientDependency).IsAssignableFrom(w)
+                                                            || typeof(ISingletonDependency).IsAssignableFrom(w)
+                                                            || typeof(IScopedDependency).IsAssignableFrom(w)
+                                                            || typeof(ISingletonSelfDependency).IsAssignableFrom(w)
+                                                            || typeof(ITransientSelfDependency).IsAssignableFrom(w)
+                                                            || typeof(IScopedSelfDependency).IsAssignableFrom(w))
+                                                           && !w.IsInterface
+                                                           && !w.IsAbstract
+                                                           && (
+                                                               (
+                                                                   Attribute.IsDefined(w,
+                                                                       typeof(InjectAppNameAttribute))
+                                                                   && Attribute.GetCustomAttribute(w,
+                                                                           typeof(InjectAppNameAttribute)) is
+                                                                       InjectAppNameAttribute attr
+                                                                   && attr.Tags.Contains(appName)
+                                                               )
+                                                               || !Attribute.IsDefined(w,
+                                                                   typeof(InjectAppNameAttribute))
+                                                           )
+                )
+                .Select(x => new TypeOfLifeCycle(x));
+
+            if (!pureObjects.Any())
+                return;
+
+            foreach (var dependencyObject in pureObjects)
             {
+                if (GetLifeCycle.TryGetValue(dependencyObject.LifeCycle,
+                        out Action<Type, IServiceCollection> _method))
+                    _method.Invoke(dependencyObject.ImplementationType, Services);
+                else
+                    throw new ArgumentNullException(
+                        $"LifeCycle : {dependencyObject.LifeCycle.FullName} \r\n ImplementationType: {dependencyObject.ImplementationType.FullName}",
+                        "Ioc yükleme esnasında hata oluşmuştur.");
+            }
+        }
+
+        /// <summary>
+        /// IConfigurationOptions interface impelement eden option class'ları tüm projede aranıp Configuration yapısına inject ediliyoır.
+        /// </summary>
+        private void ConfigureOptionsCore()
+        {
+            var optionTypes = AppDomain.Instance.GetAllAssemblies().SelectMany(s => s.ExportedTypes).Where(x =>
+                !x.IsAbstract && x.IsClass && typeof(IConfigurationOptions).IsAssignableFrom(x)).ToArray();
+
+            foreach (var options in optionTypes)
+            {
+                typeof(OptionsConfigurationServiceCollectionExtensions).GetMethods().First()
+                    .MakeGenericMethod(options)
+                    .Invoke(Configuration, new object[] { Services, Configuration.GetSection(options.Name) });
             }
 
-            private readonly IDictionary<Type, Action<Type, IServiceCollection>> GetLifeCycle =
-               new Dictionary<Type, Action<Type, IServiceCollection>> {
-                { typeof(ISingletonDependency), (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces.Where(m => m != typeof(ISingletonDependency) && !m.Namespace.Equals("fi.Framework") && !m.Namespace.Contains("System")).ToList().ForEach(i => service.AddSingleton(i, implementationType)) },
-                { typeof(IScopedDependency), (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces.Where(m => m != typeof(IScopedDependency) && !m.Namespace.Equals("fi.Framework") && !m.Namespace.Contains("System")).ToList().ForEach(i => service.AddScoped(i, implementationType)) },
-                { typeof(ITransientDependency), (implementationType, service) => implementationType.GetTypeInfo().ImplementedInterfaces.Where(m => m != typeof(ITransientDependency) && !m.Namespace.Equals("fi.Framework") && !m.Namespace.Contains("System")).ToList().ForEach(i => service.AddTransient(i, implementationType)) },
-                { typeof(IScopedSelfDependency), (implementationType, service) => service.AddScoped(implementationType) },
-                { typeof(ISingletonSelfDependency), (implementationType, service) => service.AddSingleton(implementationType) },
-                { typeof(ITransientSelfDependency), (implementationType, service) => service.AddTransient(implementationType) }
-                };
-
-            internal IServiceCollection Services { get; private set; }
-            internal IConfiguration Configuration { get; private set; }
-
-            /// <summary>
-            /// .Net in standart IOC  kullanımı için geliştirilmiştir.
-            /// </summary>
-            /// <param name="services"></param>
-            /// <param name="configuration"></param>
-            public void Start(IServiceCollection services, IConfiguration configuration)
+            foreach (var type in optionTypes)
             {
-                Services = services;
-                Configuration = configuration;
-                RegisterIntefaceBasedTypes();
-                ConfigureOptionsCore();
+                Services.AddSingleton(type, resolver =>
+                {
+                    var optionType = typeof(IOptions<>).MakeGenericType(type);
+                    return resolver.GetService(optionType).GetValue(nameof(IOptions<dynamic>.Value));
+                });
             }
-            /// <summary>
-            /// .Net in standart Worker ları için geliştirilmiştir.
-            /// </summary>
-            /// <param name="hostContext"></param>
-            /// <param name="services"></param>
-            public void Start(HostBuilderContext hostContext, IServiceCollection services)
+        }
+
+        /// <summary>
+        /// IHostedService interface implement eden tüm nesneleri hostservice olarak IOC ye inject ediyor.
+        /// </summary>
+        private void RegisterHostingWorker()
+        {
+            MethodInfo methodInfo =
+                typeof(ServiceCollectionHostedServiceExtensions)
+                    .GetMethods()
+                    .FirstOrDefault(p =>
+                        p.Name == nameof(ServiceCollectionHostedServiceExtensions.AddHostedService));
+
+            if (methodInfo == null)
+                throw new Exception(
+                    $"'{nameof(IServiceCollection)}' içindeki '{nameof(ServiceCollectionHostedServiceExtensions.AddHostedService)}' extension methodu bulunamamıştır.");
+
+            IEnumerable<Type> hostedServicesFromAssemblies = AppDomain.Instance.GetAllAssemblies()
+                .SelectMany(a => a.DefinedTypes)
+                .Where(x => !x.IsAbstract && x.IsClass && x.GetInterfaces().Contains(typeof(IHostedService)))
+                .Select(p => p.AsType());
+
+            foreach (Type hostedService in hostedServicesFromAssemblies)
             {
-                Services = services;
-                Configuration = hostContext.Configuration;
-                RegisterIntefaceBasedTypes();
-                ConfigureOptionsCore();
-                RegisterHostingWorker();
-            }
-
-            /// <summary>
-            /// ITransientDependency,IScopedDependency veya ISingletonDependency nesneleri konteynıra ekliyor.
-            /// </summary>
-            private void RegisterIntefaceBasedTypes()
-            {
-                var dependencyObjects = AppDomain.Current.GetAllAssemblies().SelectMany(s => s.DefinedTypes.Where(w => !w.IsAbstract && w.IsClass).Select(sm => sm));
-
-                var pureObjects = dependencyObjects.Where(w => (typeof(ITransientDependency).IsAssignableFrom(w)
-                                            || typeof(ISingletonDependency).IsAssignableFrom(w)
-                                            || typeof(IScopedDependency).IsAssignableFrom(w)
-                                            || typeof(ISingletonSelfDependency).IsAssignableFrom(w)
-                                            || typeof(ITransientSelfDependency).IsAssignableFrom(w)
-                                            || typeof(IScopedSelfDependency).IsAssignableFrom(w))
-                                        && !w.IsInterface
-                                        && !w.IsAbstract)
-                                  .Select(x => new TypeOfLifeCycle(x));
-
-                if (!pureObjects.Any())
-                    return;
-
-                foreach (var dependencyObject in pureObjects)
+                if (typeof(IHostedService).IsAssignableFrom(hostedService))
                 {
-                    if (GetLifeCycle.TryGetValue(dependencyObject.LifeCycle, out Action<Type, IServiceCollection> _method))
-                        _method.Invoke(dependencyObject.ImplementationType, Services);
-                    else
-                        throw new ArgumentNullException($"LifeCycle : {dependencyObject.LifeCycle.FullName} \r\n ImplementationType: {dependencyObject.ImplementationType.FullName}", "Ioc yükleme esnasında hata oluşmuştur.");
-                }
-            }
-            /// <summary>
-            /// IConfigurationOptions interface impelement eden option class'ları tüm projede aranıp Configuration yapısına inject ediliyoır.
-            /// </summary>
-            private void ConfigureOptionsCore()
-            {
-                var optionTypes = new List<Type>();
-                foreach (var item in AppDomain.Current.GetAllAssemblies())
-                {
-                    optionTypes.AddRange(item.ExportedTypes);
-                }
-                optionTypes = optionTypes.Where(x => !x.IsAbstract && x.IsClass && typeof(IConfigurationOptions).IsAssignableFrom(x)).ToList();
-                foreach (var options in optionTypes)
-                {
-                    typeof(OptionsConfigurationServiceCollectionExtensions).GetMethods().First()
-                        .MakeGenericMethod(options)
-                        .Invoke(Configuration, new object[] { Services, Configuration.GetSection(options.Name) });
-                }
-                foreach (var type in optionTypes)
-                {
-                    Services.AddSingleton(type, resolver =>
-                    {
-                        var optionType = typeof(IOptions<>).MakeGenericType(type);
-                        return resolver.GetService(optionType).GetValue(nameof(IOptions<dynamic>.Value));
-                    });
-                }
-            }
-            /// <summary>
-            /// IHostedService interface implement eden tüm nesneleri hostservice olarak IOC ye inject ediyor.
-            /// </summary>
-            private void RegisterHostingWorker()
-            {
-                MethodInfo methodInfo =
-             typeof(ServiceCollectionHostedServiceExtensions)
-             .GetMethods()
-             .FirstOrDefault(p => p.Name == nameof(ServiceCollectionHostedServiceExtensions.AddHostedService));
-
-                if (methodInfo == null)
-                    throw new Exception($"'{nameof(IServiceCollection)}' içindeki '{nameof(ServiceCollectionHostedServiceExtensions.AddHostedService)}' extension methodu bulunamamıştır.");
-
-                IEnumerable<Type> hostedServices_FromAssemblies = AppDomain.Current.GetAllAssemblies().SelectMany(a => a.DefinedTypes).Where(x => !x.IsAbstract && x.IsClass && x.GetInterfaces().Contains(typeof(IHostedService))).Select(p => p.AsType());
-
-                foreach (Type hostedService in hostedServices_FromAssemblies)
-                {
-                    if (typeof(IHostedService).IsAssignableFrom(hostedService))
-                    {
-                        var genericMethod_AddHostedService = methodInfo.MakeGenericMethod(hostedService);
-                        _ = genericMethod_AddHostedService.Invoke(obj: null, parameters: new object[] { Services });
-                    }
+                    var genericMethodAddHostedService = methodInfo.MakeGenericMethod(hostedService);
+                    _ = genericMethodAddHostedService.Invoke(obj: null, parameters: new object[] { Services });
                 }
             }
         }
--- fi.Ioc/Utilities/AppDomain.cs
+++ fi.Ioc/Utilities/AppDomain.cs
@@ -4,83 +4,106 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using fi.Core.AbstractUtility;
 
 
-namespace fi.Ioc
+namespace fi.Ioc;
+
+internal class AppDomain : LazySingleton<AppDomain>
 {
-    internal class AppDomain
+    private readonly ICollection<Assembly> _assemblies;
+
+    private AppDomain()
     {
-        private static readonly Lazy<AppDomain> lazy = new(() => new AppDomain());
-        public static AppDomain Current => lazy.Value;
-        private readonly ICollection<Assembly> assemblies;
-        private AppDomain() { assemblies = new HashSet<Assembly>(); }
+        _assemblies = new HashSet<Assembly>();
+    }
 
-        public ICollection<Assembly> GetAllAssemblies()
+    public ICollection<Assembly> GetAllAssemblies()
+    {
+        if (_assemblies.Count > 0)
+            return _assemblies;
+
+        var path = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? string.Empty;
+
+        var files = Directory.GetFiles(path, "*.dll")
+            .Where(i => !new[] { "Microsoft.", "System." }.Any(j => i.Contains(j)));
+
+        foreach (string item in files)
         {
-            if (assemblies.Any())
-                return assemblies;
-
-            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-
-            var files = Directory.GetFiles(path, "*.dll").Where(i => !new string[] { "Microsoft.", "System." }.Any(j => i.Contains(j)));
-
-            foreach (string item in files)
+            try
             {
-                try
-                {
-                    Assembly assembly = Assembly.LoadFrom(item);
-                    assemblies.Add(assembly);
-                }
-                catch { }
+                Assembly assembly = Assembly.LoadFrom(item);
+                _assemblies.Add(assembly);
             }
-
-            return assemblies;
-
-
-            #region Yontem 1: Current projede ilgili dll kullanilmissa ekler.
-            //if (assemblies != null)
-            //    return assemblies;
-            //assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
-            //assemblies.Add(Assembly.GetEntryAssembly());
-            //return assemblies;
-            #endregion Yontem 1: Current projede ilgili dll kullanilmissa ekler.
-
-
-            #region Yontem 2: tum assembly'de recursive gezmek. Not asagidaki kod recursive degildir sadece bir alta inmisdir.
-            //if (assemblies != null)
-            //    return assemblies;
-
-            //assemblies = new List<Assembly> { Assembly.GetEntryAssembly() };
-
-            //Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList().ForEach(assembly =>
-            //{
-            //    if (!assemblies.Any(u => u.FullName == assembly.FullName))
-            //        assemblies.Add(assembly);
-
-            //    assembly.GetReferencedAssemblies().Select(Assembly.Load).ToList().ForEach(innerAssembly =>
-            //    {
-            //        if (!assemblies.Any(u => u.FullName == innerAssembly.FullName))
-            //            assemblies.Add(innerAssembly);
-            //    });
-            //});
-
-            //return assemblies;
-            #endregion Yontem 2: tum assembly'de recursive gezmek. Not yukaridaki kod recursive degildir sadece bir alta inmisdir.
+            catch
+            {
+            }
         }
+
+        return _assemblies;
+
+        #region Yontem 1: Current projede ilgili dll kullanilmissa ekler.
+
+        //if (assemblies != null)
+        //    return assemblies;
+        //assemblies = Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        //assemblies.Add(Assembly.GetEntryAssembly());
+        //return assemblies;
+
+        #endregion Yontem 1: Current projede ilgili dll kullanilmissa ekler.
+
+
+        #region Yontem 2: tum assembly'de recursive gezmek. Not asagidaki kod recursive degildir sadece bir alta inmisdir.
+
+        //if (assemblies != null)
+        //    return assemblies;
+
+        //assemblies = new List<Assembly> { Assembly.GetEntryAssembly() };
+
+        //Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList().ForEach(assembly =>
+        //{
+        //    if (!assemblies.Any(u => u.FullName == assembly.FullName))
+        //        assemblies.Add(assembly);
+
+        //    assembly.GetReferencedAssemblies().Select(Assembly.Load).ToList().ForEach(innerAssembly =>
+        //    {
+        //        if (!assemblies.Any(u => u.FullName == innerAssembly.FullName))
+        //            assemblies.Add(innerAssembly);
+        //    });
+        //});
+
+        //return assemblies;
+
+        #endregion Yontem 2: tum assembly'de recursive gezmek. Not yukaridaki kod recursive degildir sadece bir alta inmisdir.
+    }
+}
+
+internal class TypeOfLifeCycle
+{
+    public TypeOfLifeCycle(Type implementationType)
+    {
+        ImplementationType = implementationType;
     }
 
-    internal class TypeOfLifeCycle
-    {
-        public TypeOfLifeCycle(Type implementationType)
-        {
-            ImplementationType = implementationType;
-        }
-        public Type ImplementationType { get; }
-        public Type LifeCycle => typeof(ITransientDependency).IsAssignableFrom(ImplementationType) ? typeof(ITransientDependency) : typeof(IScopedDependency).IsAssignableFrom(ImplementationType) ? typeof(IScopedDependency) : typeof(ISingletonDependency).IsAssignableFrom(ImplementationType) ? typeof(ISingletonDependency) : typeof(IScopedSelfDependency).IsAssignableFrom(ImplementationType) ? typeof(IScopedSelfDependency) : typeof(ISingletonSelfDependency).IsAssignableFrom(ImplementationType) ? typeof(ISingletonSelfDependency) : typeof(ITransientSelfDependency).IsAssignableFrom(ImplementationType) ? typeof(ITransientSelfDependency) : null;
-    }
+    public Type ImplementationType { get; }
 
-    internal static class ObjectExtensions
-    {
-        internal static object GetValue(this object obj, string propertyNameToGetValueFrom) => obj.GetType().GetProperty(propertyNameToGetValueFrom)?.GetValue(obj, null);
-    }
+    public Type LifeCycle => typeof(ITransientDependency).IsAssignableFrom(ImplementationType)
+        ? typeof(ITransientDependency)
+        : typeof(IScopedDependency).IsAssignableFrom(ImplementationType)
+            ? typeof(IScopedDependency)
+            : typeof(ISingletonDependency).IsAssignableFrom(ImplementationType)
+                ? typeof(ISingletonDependency)
+                : typeof(IScopedSelfDependency).IsAssignableFrom(ImplementationType)
+                    ? typeof(IScopedSelfDependency)
+                    : typeof(ISingletonSelfDependency).IsAssignableFrom(ImplementationType)
+                        ? typeof(ISingletonSelfDependency)
+                        : typeof(ITransientSelfDependency).IsAssignableFrom(ImplementationType)
+                            ? typeof(ITransientSelfDependency)
+                            : null;
+}
+
+internal static class ObjectExtensions
+{
+    internal static object GetValue(this object obj, string propertyNameToGetValueFrom) =>
+        obj.GetType().GetProperty(propertyNameToGetValueFrom)?.GetValue(obj, null);
 }
